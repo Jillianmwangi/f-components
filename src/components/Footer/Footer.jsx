14d35e1c415a6f06e1aebae4e6256e40083d498d
Jillianmwangi <mwangyjill@gmail.com>
2023-02-02 14:38:23 +0300

    commit recent changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css"
 
 function Footer() {
   return (
-    <div>
+    <div className="bottom">
       <p className="foot"> Footer goes here</p>
       <span>
         <button className="btn">Back to the top</button>
