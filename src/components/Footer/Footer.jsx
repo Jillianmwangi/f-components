d96323cefd367888c2e58dc669919f66690a92d1
Jillianmwangi <mwangyjill@gmail.com>
2023-02-02 13:24:57 +0300

    commit style updates

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Footer.css"
 
 function Footer() {
   return (
     <div>
-      <p>Footer goes here</p>
+      <p className="foot"> Footer goes here</p>
       <span>
-        <button>Back to the top</button>
+        <button className="btn">Back to the top</button>
       </span>
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Main.css"
 
 function Main() {
   return (
     <div>
-      <p>
+      <p className="text">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae at est
         asperiores ipsa. Eius totam voluptates fugiat pariatur ab,
         <br />
